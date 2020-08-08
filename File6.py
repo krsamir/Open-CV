a246e8e19e1f3251cd87bb858f3ac0474bd0900c
krsamir <samir1741012321@gmail.com>
2020-08-08 09:49:46 +0530

    Inserted Images

--- File6.py
+++ File6.py
@@ -7,5 +7,8 @@ imgHor = np.hstack((img, img))
 imgVer = np.vstack((img, img))
 cv2.imshow("Image Horizontal", imgHor)
 cv2.imshow("Image Vertical", imgVer)
+cv2.imwrite('./Horizontal Image.jpeg', imgHor)
+cv2.imwrite('./Vertical Image.jpeg', imgVer)
+
 
 cv2.waitKey(0)
